b4dddac1301243ff5ed8742d041558e1cfcd00a9
Nataleesha <frojlan@gmail.com>
2023-09-14 04:28:25 +0300

    added pagination to breeds page

--- src/components/BreedsFilter/BreedsFilter.jsx
+++ src/components/BreedsFilter/BreedsFilter.jsx
@@ -19,16 +19,20 @@ import {
   ImageOverlay,
   TextOverlay,
   Group,
+  PaginationContainer,
+  Pagination,
 } from "./BreedsFilter.styled";
 
 const BreedsFilter = ({ breeds }) => {
   const [selectedBreed, setSelectedBreed] = useState("");
   const [limit, setLimit] = useState(5);
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(0);
+  const [noMoreResults, setNoMoreResults] = useState(false);
 
   useEffect(() => {
     const getImages = async () => {
-      const res = await getData("images/search?limit=20&has_breeds=1");
+      const res = await getData("images/search?limit=5&page=0&has_breeds=1");
       setImages(res.data);
     };
 
@@ -41,9 +45,9 @@ const BreedsFilter = ({ breeds }) => {
       setImages([]);
       const res = e.target.value
         ? await getData(
-            `images/search?limit=20&has_breeds=1&breed_ids=${e.target.value}`
+            `images/search?limit=100&has_breeds=1&breed_ids=${e.target.value}`
           )
-        : await getData("images/search?limit=20&has_breeds=1");
+        : await getData("images/search?limit=100&has_breeds=1");
       setImages(res.data);
     };
     breedImages();
@@ -51,6 +55,7 @@ const BreedsFilter = ({ breeds }) => {
 
   const handleLimitSelect = (e) => {
     setLimit(e.target.value);
+    setPage(0);
   };
 
   const abcSort = () => {
@@ -69,6 +74,26 @@ const BreedsFilter = ({ breeds }) => {
       return breedA > breedB ? -1 : breedA < breedB ? 1 : 0;
     });
     setImages(sorted);
+  };
+
+  const handleNextPage = async () => {
+    const res = await getData(
+      `images/search?limit=${limit}&page=${page}&has_breeds=1`
+    );
+    if (res.length < limit) {
+      setNoMoreResults(true);
+    }
+    setImages(res.data);
+    setPage((page) => page + 1);
+  };
+
+  const handlePrevPage = async () => {
+    const res = await getData(
+      `images/search?limit=${limit}&page=${page}&has_breeds=1`
+    );
+    setImages(res.data);
+    setPage((page) => page - 1);
+    setNoMoreResults(false);
   };
 
   return (
@@ -156,6 +181,42 @@ const BreedsFilter = ({ breeds }) => {
           })
         )}
       </GridContainer>
+      <PaginationContainer>
+        <Pagination type="button" disabled={!page} onClick={handlePrevPage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M9.99984 14C9.74397 14 9.48797 13.9023 9.29297 13.707L4.29297 8.70703C3.90234 8.31641 3.90234 7.68359 4.29297 7.29297L9.29297 2.29297C9.68359 1.90234 10.3164 1.90234 10.707 2.29297C11.0977 2.68359 11.0977 3.31641 10.707 3.70703L6.41391 8L10.7077 12.2937C11.0983 12.6844 11.0983 13.3172 10.7077 13.7078C10.5123 13.9031 10.2561 14 9.99984 14Z"
+              fill="currentColor"
+            />
+          </svg>
+          Prev
+        </Pagination>
+        <Pagination
+          type="button"
+          disabled={noMoreResults}
+          onClick={handleNextPage}
+        >
+          Next
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M6.00016 2C6.25603 2 6.51203 2.09766 6.70703 2.29297L11.707 7.29297C12.0977 7.68359 12.0977 8.31641 11.707 8.70703L6.70703 13.707C6.31641 14.0977 5.68359 14.0977 5.29297 13.707C4.90234 13.3164 4.90234 12.6836 5.29297 12.293L9.58609 8L5.29234 3.70625C4.90172 3.31563 4.90172 2.68281 5.29234 2.29219C5.48766 2.09688 5.74391 2 6.00016 2Z"
+              fill="currentColor"
+            />
+          </svg>
+        </Pagination>
+      </PaginationContainer>
     </>
   );
 };
--- src/components/BreedsFilter/BreedsFilter.styled.js
+++ src/components/BreedsFilter/BreedsFilter.styled.js
@@ -174,3 +174,42 @@ export const TextOverlay = styled.p`
   line-height: 24px;
   padding: 5px 0;
 `;
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`;
+
+export const Pagination = styled.button`
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  width: 122px;
+  height: 40px;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  background-color: var(--primary-light);
+  color: var(--primary);
+  border-radius: 10px;
+  border-color: transparent;
+  transition: 0.5s ease;
+
+  &:hover {
+    transition: 0.5s ease;
+    color: var(--white);
+    background-color: var(--primary);
+  }
+
+  &[disabled] {
+    background-color: var(--background-color);
+    color: var(--text-light);
+    cursor: default;
+  }
+`;
